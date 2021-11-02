2ddc61fb1cdaef1dd533c8262df9ed763b4b028d
krof.roman.2018 <krof.roman.2018@UC313PC14>
2021-11-02 13:55:56 +0100

    Přidejte soubory projektu.

---
+++ XamarinTest/XamarinTest/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+
+namespace XamarinTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            Debug.WriteLine("The application has started.");
+            DateTime dt;
+            dt = DateTime.Now;
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+            Debug.WriteLine("The application is asleep.");
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+            Debug.WriteLine("The application resumes.");
+        }
+    }
+}
---
+++ XamarinTest/XamarinTest/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace XamarinTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Klikač", MethodBase.GetCurrentMethod().Name, "Zavřít");
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            DisplayAlert("Časovač", Convert.ToString(Application.Current.Properties["FirstRun"]), "Zavřít");
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            Application.Current.Properties.Add("FirstRun", DateTime.Now);
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Entry entry = new Entry() { Text = "Zadej jméno" };
+            MainStackLayout.Children.Add(entry);
+        }
+    }
+}
